e90ec6a4739e0b700ef6450941c9b5848bc2df8e
Vishnu Priya <vishnupriya@ip-172-31-16-94.us-west-2.compute.internal>
2015-11-20 13:05:49 -0800

    Changed the source code

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! This is Mohan." );
+        System.out.println( "Hello World! This is Vishnu's branch" );
     }
 }
